fb59ac31224bbec00b6a4bc8d8296f4f672d9b1a
李明柠 <limingning@limingningdeMacBook-Air.local>
2025-03-25 18:49:50 +0800

    make some comments in lib_man_sys.c

--- lab1/lib_man_sys.c
+++ lab1/lib_man_sys.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-// 定义图书信息结构体
+/* Define the book structure. */
 typedef struct {
     char bookId[20]; // 书号
     char title[100]; // 书名
@@ -10,13 +10,13 @@ typedef struct {
     int stock;       // 库存数量
 } Book;
 
-// 定义链表节点结构体
+/* Define the linked list node structure. */
 typedef struct Node {
     Book book;
     struct Node *next;
 } Node;
 
-// 创建新节点
+/* Create new node. */
 Node* CreateNode(Book book) {
     Node *p = (Node*)malloc(sizeof(Node));
     p->book = book;
@@ -24,7 +24,7 @@ Node* CreateNode(Book book) {
     return p;
 }
 
-// 在链表尾部插入图书信息
+/* Insert book information at the end of the linked list. */
 void InsertBook(Node **head, Book book) {
     Node *last = CreateNode(book);
     if (*head == NULL) {  /* Easy to make mistake */
@@ -40,7 +40,7 @@ void InsertBook(Node **head, Book book) {
     return ;
 }
 
-// 根据书号删除图书信息
+/* Delete book information according to ISBN number. */
 int DeleteBook(Node **head, char bookId[]) {
     Node *p = *head;
     if (*head == NULL) {
@@ -79,7 +79,7 @@ int DeleteBook(Node **head, char bookId[]) {
     }
 }
 
-// 根据书号修改图书库存数量
+/*  */
 int UpdateStock(Node *head, char bookId[], int newStock) {
     for (Node *p = head; p->next != NULL; p = p->next) {
         if (strcmp(p->book.bookId,bookId) == 0) {
@@ -90,7 +90,7 @@ int UpdateStock(Node *head, char bookId[], int newStock) {
     return 1;
 }
 
-// 根据书号查找图书信息
+/* Search for book information by ISBN. */
 Node* FindBook(Node *head, char bookId[]) {
     for (Node *p = head; p->next != NULL; p = p->next) {
         if (strcmp(p->book.bookId,bookId) == 0) {
@@ -100,7 +100,7 @@ Node* FindBook(Node *head, char bookId[]) {
     return NULL;
 }
 
-// 遍历并输出所有图书信息
+/* Traverse and output all book information. */
 void TraverseList(Node *head) {
     for (Node *p = head; p != NULL; p = p->next) {
         printf("书号：%s,书名：%s,作者：%s,库存：%d",p->book.bookId,p->book.title,p->book.author,p->book.stock);
@@ -111,9 +111,8 @@ void TraverseList(Node *head) {
     return ;
 }
 
-// 主函数
 int main() {
-    Node *head = NULL; // 链表头节点
+    Node *head = NULL;
     char bookId_find[4], bookId_update[4], bookId_delete[4];
     int num;
     scanf("%s", bookId_find);
@@ -121,7 +120,7 @@ int main() {
     scanf("%d", &num);
     scanf("%s", bookId_delete);
 
-    // 添加图书信息
+    /* Add the information of books. */
     Book book1 = {"001", "C程序设计", "谭浩强", 10};
     Book book2 = {"002", "数据结构", "严蔚敏", 5};
     Book book3 = {"003", "算法导论", "Thomas H. Cormen", 3};
@@ -129,23 +128,23 @@ int main() {
     InsertBook(&head, book2);
     InsertBook(&head, book3);
 
-    // 查找图书信息
+    /* Find the imformation of books. */
     Node *bookFind = FindBook(head, bookId_find);
     printf("查找的图书信息：\n");
     printf("书号：%s,书名：%s,作者：%s,库存：%d\n",bookFind->book.bookId,bookFind->book.title,bookFind->book.author,bookFind->book.stock);
 
 
-    // 修改图书库存数量
+    /* Change the stock of books. */
     if (!UpdateStock(head, bookId_update, num)) {
         printf("图书%s的库存数量已修改为%d！\n",bookId_update,num);
     }
 
-    // 删除图书信息
+    /* Delete the information of the books. */
     if (!DeleteBook(&head, bookId_delete)) {
         printf("图书%s删除成功！\n",bookId_delete);
     }
 
-    // 遍历输出图书列表
+    /* Traverse and output all book information. */
     printf("图书列表：\n");
     TraverseList(head);
 
